07da00812ecdb9e60da6bf5c94856e1435a21964
JaydenLee1116 <vv55adss@gmail.com>
2022-10-20 02:26:34 +0900

    learn: 타입스크립트 타입 지정

---
+++ index.js
@@ -0,0 +1,6 @@
+let num = 5;
+function test(x) {
+    return x * 2;
+}
+let obj = { name: 'jay', age: 31 };
+let temp = 1;
---
+++ index.ts
@@ -0,0 +1,11 @@
+let num: number = 5;
+
+function test(x: number): number {
+  return x * 2;
+}
+
+let obj: { name: string; age: number } = { name: 'jay', age: 31 };
+
+type Jayden = string | number;
+
+let temp: Jayden = 1;
